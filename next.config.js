4f5749d7204073412b1d9ce2a647b514a7e44f1f
Filip FICO <fitz993m@gmail.com>
2020-08-09 16:42:59 +0200

    23. test

--- next.config.js
+++ next.config.js
@@ -1,28 +1,38 @@
 const path = require("path");
-const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
 
 const nextConfig = {
   trailingSlash: false,
   poweredByHeader: false,
   webpack(config, options) {
+    const { isServer } = options;
     config.resolve.alias = {
       ...config.resolve.alias,
       "@components": path.resolve("./components"),
       "@public": path.resolve("./public"),
       "@redux": path.resolve("./redux"),
     };
+
     config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      loader: "file-loader",
-      options: {
-        outputPath: "../public/",
-        publicPath: "assets/",
-      },
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name]-[hash:8].[ext]",
+            publicPath: (url) => {
+              return `/_next/static/css/${url}`;
+            },
+            outputPath: `${isServer ? "../" : ""}static/css/`,
+            esModule: false,
+          },
+        },
+      ],
     });
 
     return config;
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins(withImages, nextConfig);
