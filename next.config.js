dd4573cd5d7f1fc383f9a805e7ef056ede8c8663
Filip FICO <fitz993m@gmail.com>
2020-08-09 14:31:09 +0200

    7. updated config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 
@@ -7,14 +8,12 @@ const nextConfig = {
   webpack(config, options) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@components": path.resolve("./components"),
+
       "@public": path.resolve("./public"),
-      "@redux": path.resolve("./redux"),
-      "@utils": path.resolve("./utils"),
     };
 
     return config;
   },
 };
 
-module.exports = withPlugins([withCSS], nextConfig);
+module.exports = nextConfig;
