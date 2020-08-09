3d17dc1c92470f773ebbaba9980911081d019f97
Filip FICO <fitz993m@gmail.com>
2020-08-09 14:25:36 +0200

    6 new config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 const withImages = require("next-images");
-module.exports = withImages({
+const nextConfig = {
+  trailingSlash: false,
+  poweredByHeader: false,
   webpack(config, options) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve("./components"),
+      "@public": path.resolve("./public"),
+      "@redux": path.resolve("./redux"),
+      "@utils": path.resolve("./utils"),
+    };
+
     return config;
   },
-});
+};
+
+module.exports = withPlugins([withCSS], nextConfig);
