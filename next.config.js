f116c3b5114e2edef9fe430ded2b9242994c1297
Filip FICO <fitz993m@gmail.com>
2020-08-09 17:03:06 +0200

    24. config test

--- next.config.js
+++ next.config.js
@@ -15,25 +15,8 @@ const nextConfig = {
     };
 
     config.module.rules.push({
-<<<<<<< HEAD
       test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
       loader: require.resolve("url-loader"),
-=======
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[name]-[hash:8].[ext]",
-            publicPath: (url) => {
-              return `/_next/static/css/${url}`;
-            },
-            outputPath: `${isServer ? "../" : ""}static/css/`,
-            esModule: false,
-          },
-        },
-      ],
->>>>>>> 4f5749d7204073412b1d9ce2a647b514a7e44f1f
     });
 
     return config;
